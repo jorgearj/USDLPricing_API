cb78eb9429a989a3401a1c63d2d8032d69f4fa7f
dguedesb <danielgbarrigas@hotmail.com>
2014-06-11 16:53:24 +0100

    Update APIUsageExample.java

--- SPIN_API/src/Examples/APIUsageExample.java
+++ SPIN_API/src/Examples/APIUsageExample.java
@@ -1,8 +1,22 @@
 /* Copyright (C) 2013 Jorge Araújo. All rights reserved.
 * 
-* This program and the accompanying materials are made available under
-* the terms of the Common Public License v1.0 which accompanies this distribution,
-* and is available at http://www.eclipse.org/legal/cpl-v10.html
+ *  ----------------------------------------------------------------------------------------
+ *  This file is part of LinkedUSDLPricingAPI.
+ *
+ *  LinkedUSDLPricingAPI is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ * 
+ *  LinkedUSDLPricingAPI is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with LinkedUSDLPricingAPI.  If not, see <http://www.gnu.org/licenses/>.
+ *  ---------------------------------------------------------------------------------------
+ *
 * 
 * Id: test.java, Project: CloudAid, 13 Apr 2013 Author: Jorge Araújo
 */
