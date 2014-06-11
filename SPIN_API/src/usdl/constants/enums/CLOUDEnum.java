510069d7471458490dd29beaa624e0542e18e807
dguedesb <danielgbarrigas@hotmail.com>
2014-06-11 16:59:44 +0100

    Update CLOUDEnum.java

--- SPIN_API/src/usdl/constants/enums/CLOUDEnum.java
+++ SPIN_API/src/usdl/constants/enums/CLOUDEnum.java
@@ -8,6 +8,24 @@ import com.hp.hpl.jena.rdf.model.Resource;
  * @author  Daniel Barrigas
  * @author Jorge Araujo
  * @version 1.0, March 10
+ * 
+ * 
+ *  *  ----------------------------------------------------------------------------------------
+ *  This file is part of LinkedUSDLPricingAPI.
+ *
+ *  LinkedUSDLPricingAPI is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ * 
+ *  LinkedUSDLPricingAPI is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with LinkedUSDLPricingAPI.  If not, see <http://www.gnu.org/licenses/>.
+ *  ---------------------------------------------------------------------------------------
  */
 public enum CLOUDEnum {
 
