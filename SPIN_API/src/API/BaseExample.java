892d5ea82717d52a0310fc9334b3c2e3c78af419
dguedesb <danielgbarrigas@hotmail.com>
2014-06-11 16:51:23 +0100

    Update BaseExample.java

--- SPIN_API/src/API/BaseExample.java
+++ SPIN_API/src/API/BaseExample.java
@@ -5,6 +5,22 @@
  *  A simple code to create a SPIN function with 3 variables, 1 Constants and 2 Usage, using the TopBraid Composer API and then call it to get the dynamic result
  *  Used formula = (U + U) * C -> Heroku use case
  *  It also lists the functions variables, it's usage variables and requests to the user to insert a value for each usage variable. 
+ *  ----------------------------------------------------------------------------------------
+ *  This file is part of LinkedUSDLPricingAPI.
+ *
+ *  LinkedUSDLPricingAPI is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ * 
+ *  LinkedUSDLPricingAPI is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with LinkedUSDLPricingAPI.  If not, see <http://www.gnu.org/licenses/>.
+ *  ---------------------------------------------------------------------------------------
  */
 
 package API;
